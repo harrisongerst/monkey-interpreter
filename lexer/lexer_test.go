a773c4265e44a6939da4f98db0979d37e63fb98f
harrisongerst <harrisongerstenlauer@gmail.com>
2024-06-02 13:13:09 -0400

    extend tests further

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -119,13 +119,25 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
+
 	let ten = 10;
+	
 	let add = fn(x, y) {
 	x + y;
 	};
+	
 	let result = add(five, ten);
 	!-/*5;
 	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
+
+	10 == 10;
+	10 != 9;
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -179,6 +191,23 @@ func TestNextToken(t *testing.T) {
 		{token.GT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 	l := New(input)
